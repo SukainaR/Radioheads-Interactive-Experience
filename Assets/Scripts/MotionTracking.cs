6d3cc0a5dad830d1c86b1915c9a492f62067392f
SukainaR <rizwansukaina@gmail.com>
2024-05-14 13:52:39 +0100

    Motion Capturing Integration

---
+++ Assets/Scripts/MotionTracking.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionTracking : MonoBehaviour
+{
+    public ReceiveDataFromTracking udpReceive;
+    public GameObject[] bodyPoints;
+
+    public string[] points;
+
+    // Update is called once per frame
+    void Update()
+    {
+        string data = udpReceive.data;
+        data = data.Remove(0, 2);
+        data = data.Remove(data.Length - 2, 2);
+
+        points = data.Split(',');
+
+        for (int i = 0; i < 32; i++)
+        {
+            float x = float.Parse(points[i * 3]) / 100;
+            float y = float.Parse(points[i * 3 + 1]) / 100;
+            //float z = float.Parse(points[i * 3 + 2]) / 100;
+
+            float z = 0;
+
+            bodyPoints[i].transform.localPosition = new Vector3(x, y, z);
+        }
+
+    }
+}
